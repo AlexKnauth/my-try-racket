084a7d211b123ac4e8dd6ddc24821f2cc5928a69
AlexKnauth <alexander@knauth.org>
2015-01-02 18:07:56 -0500

    extend for multi-line exprs and multiple exprs

--- my-try-racket/main.rkt
+++ my-try-racket/main.rkt
@@ -18,6 +18,10 @@
 (define (extend-already-text! . stuff)
   (set! already-text (append already-text stuff)))
 
+(define in-progress-text "")
+(define (extend-in-progress-text! . strs)
+  (set! in-progress-text (string-append* in-progress-text strs)))
+
 (define (myresponse request)
   (define bindings (request-bindings request))
   (when (exists-binding? 'expr bindings)
@@ -25,31 +29,45 @@
     (extend-already-text! expr '(br))
     (eval-and-handle-expr! expr)
     )
-  (extend-already-text! "> ")
+  (when (string=? in-progress-text "")
+    (extend-already-text! "> "))
   (response/xexpr
    `(html (head (title "My Try-Racket Repl, in progress")
                 (style "body {font-family: monospace;}"
                        ".output {color: purple;}"
                        ".error-output {color: red;}"
                        ".value {color: blue;}"
+                       ".input {font-family: monospace; width: 100%;}"
                        ))
           (body
            (h1 "My Try-Racket Repl, in progress")
            ,@already-text
-           (form (input ((name "expr") (autofocus "true"))))
+           (form (input ((name "expr") (autofocus "true") (class "input"))))
            ))))
 
 (define (eval-and-handle-expr! expr)
-  (define lst (call-with-values (λ () (ev expr)) list))
-  (define output (get-output ev))
-  (define err-output (get-error-output ev))
-  (define (string->lsthtml str)
-    (define strs (string-split str "\n"))
-    (append-map (λ (s) `(,s (br))) strs))
-  (extend-already-text!
-   `(span ((class "output")) ,@(string->lsthtml output))
-   `(span ((class "error-output")) ,@(string->lsthtml err-output))
-   `(span () ,@(for/list ([val (in-list lst)] #:when (not (void? val)))
-                 `(span ((class "value")) ,@(string->lsthtml (~v val)))))))
+  (extend-in-progress-text! expr "\n")
+  (define (read/exn in)
+    (with-handlers ([exn:fail:read? (λ (x) x)])
+      (read-syntax 'my-try-racket-repl in)))
+  (define exprs
+    (for/list ([expr (in-port read/exn (open-input-string in-progress-text))])
+      expr))
+  (cond [(ormap exn:fail:read? exprs) (void)]
+        [else
+         (set! in-progress-text "")
+         (for ([expr (in-list exprs)])
+           (define lst (call-with-values (λ () (ev expr)) list))
+           (define output (get-output ev))
+           (define err-output (get-error-output ev))
+           (define (string->lsthtml str)
+             (define strs (string-split str "\n"))
+             (append-map (λ (s) `(,s (br))) strs))
+           (extend-already-text!
+            `(span ((class "output")) ,@(string->lsthtml output))
+            `(span ((class "error-output")) ,@(string->lsthtml err-output))
+            `(span () ,@(for/list ([val (in-list lst)] #:when (not (void? val)))
+                          `(span ((class "value")) ,@(string->lsthtml (~v val))))))
+           )]))
 
 (serve/servlet myresponse)
